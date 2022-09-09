81846b859c51510dce324f102389344b2200b7d2
Berkay Coban <hberkaycoban@gmail.com>
2022-09-09 21:18:43 +0300

    commit message

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,98 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product
+                {
+                    ProductId = 1,
+                    CategoryId = 1,
+                    ProductName = "Monitor",
+                    UnitPrice = 25,
+                    UnitsInStock = 15
+                },
+                                new Product
+                {
+                    ProductId = 2,
+                    CategoryId = 1,
+                    ProductName = "TV",
+                    UnitPrice = 100,
+                    UnitsInStock = 15
+                },
+                                                new Product
+                {
+                    ProductId = 3,
+                    CategoryId = 1,
+                    ProductName = "Keyboard",
+                    UnitPrice = 5,
+                    UnitsInStock = 15
+                },
+                                                                new Product
+                {
+                    ProductId = 4,
+                    CategoryId = 1,
+                    ProductName = "Mouse",
+                    UnitPrice = 5,
+                    UnitsInStock = 15
+                },
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(w => w.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
